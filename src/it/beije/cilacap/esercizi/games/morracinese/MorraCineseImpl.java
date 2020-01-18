50e057bf6905f0f79ee74bf74b28900ccb7c0582
Gabriele <Gabriele@GABIT001>
2020-01-18 13:05:04 +0100

    quasi concluso Morra

--- src/it/beije/cilacap/esercizi/games/morracinese/MorraCineseImpl.java
+++ src/it/beije/cilacap/esercizi/games/morracinese/MorraCineseImpl.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MorraCineseImpl implements MorraInterface {
 
 	private boolean exitGameControlVariable = false;
-	private String[] playersName = {"", ""};	
+	private String[] playersName = { "", "" };
 	private int timesMenuCalled = 0;
 
 	@SuppressWarnings("resource")
@@ -20,9 +20,9 @@ public class MorraCineseImpl implements MorraInterface {
 		System.out.println(playersName[0] + " Iniziamo Subito a Giocare:");
 		System.out.println("#################################################");
 		System.out.println("Scegli La Modalità di Gioco Che Fa Per Te:");
-		System.out.println("1 -- Player vs Player");
-		System.out.println("2 -- Player vs Computer");
-		System.out.println("3 -- Esci Dal Gioco");
+		// System.out.println("1 -- Player vs Player");
+		System.out.println("1 -- Player vs Computer");
+		System.out.println("2 -- Esci Dal Gioco");
 		System.out.println("#################################################");
 		System.out.println();
 		int choiceMenu = scan.nextInt();
@@ -31,6 +31,7 @@ public class MorraCineseImpl implements MorraInterface {
 	}
 
 	private String askName() {
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Ciao, Dimmi il tuo nome: ");
 		playersName[0] = scan.next();
@@ -46,6 +47,7 @@ public class MorraCineseImpl implements MorraInterface {
 
 	@Override
 	public void choiceOneFlow() {
+		@SuppressWarnings("resource")
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Ciao " + playersName[0] + " lascia la tastiera al tuo amico");
 		System.out.println("Ciao Amico Mi Diresti Il Tuo Nome ?");
@@ -53,30 +55,35 @@ public class MorraCineseImpl implements MorraInterface {
 		System.out.println("Ciao " + playersName[1] + " lascia la tastiera a " + playersName[0] + ".");
 		System.out.println();
 		int choicesToCompare[] = choiceScenario();
-		compara(choicesToCompare[0], choicesToCompare[1]);
-		
-		
+		Mossa mossaVincente = vinceLaMossa(choicesToCompare[0], choicesToCompare[1]);
+		System.out.println("vince il giocatoe ");
+
 	}
-	
+
 	public int[] choiceScenario() {
 		@SuppressWarnings("resource")
-		Scanner scan = new Scanner(System.in);		
-		scenario(playersName[0]);
+		Scanner scan = new Scanner(System.in);
+		onMoveMenu(playersName[0]);
 		int valueP1 = scan.nextInt();
-		scenario(playersName[1]);
+		onMoveMenu(playersName[1]);
 		int valueP2 = scan.nextInt();
 		int[] values = new int[2];
 		values[0] = valueP1;
 		values[1] = valueP2;
 		return values;
 	}
-	
-	
-	public void compara(int valueP1, int valueP2) {
-		
+
+	public Mossa vinceLaMossa(int valueP1, int valueP2) {
+
+		Mossa mossa1 = null;
+		Mossa mossa2 = null;
+		mossa1 = Mossa.prendiMossa(valueP1);
+		mossa2 = Mossa.prendiMossa(valueP2);
+		int move = mossa1.comparaMossa(mossa2);
+		return null;
 	}
-	
-	public void scenario(String nameP) {
+
+	public void onMoveMenu(String nameP) {
 		System.out.println(nameP + " senza farti vedere dall'altro giocatore scegli fra questi:");
 		System.out.println("1 -- Sasso   --");
 		System.out.println("2 -- Carta   --");
@@ -91,14 +98,10 @@ public class MorraCineseImpl implements MorraInterface {
 			choiceOneFlow();
 			break;
 		case 2:
-			choiceTwoFlow();
-			break;
-		case 3:
 			exitGameControlVariable = true;
 			break;
 		default:
-			System.out.println("immetti un valote valido: 1 - 2");
-			onMainMenu();
+			System.out.println("immetti un valore valido: 1 - 2");
 		}
 	}
 
---
+++ src/it/beije/cilacap/esercizi/games/morracinese/Mossa.java
@@ -0,0 +1,43 @@
+package it.beije.cilacap.esercizi.games.morracinese;
+
+public enum Mossa {
+
+	SASSO, CARTA, FORBICI;
+
+	public int comparaMossa(Mossa mossaAvversario) {
+		// Caso di parità
+		if (this == mossaAvversario)
+			return 0;
+
+		switch (this) {
+		case SASSO:
+			return (mossaAvversario == FORBICI ? 1 : -1);  
+		case CARTA:
+			return (mossaAvversario == SASSO ? 1 : -1);
+		case FORBICI:
+			return (mossaAvversario == CARTA ? 1 : -1);
+		}
+		return 0;
+	}
+
+	public static Mossa prendiMossa(int mossa) {
+
+		Mossa move = null;
+	do {
+		switch (mossa) {
+		case 1: move = Mossa.SASSO;
+				break;
+		case 2: move = Mossa.CARTA;
+				break;
+		case 3: move = Mossa.FORBICI;
+				break;
+		default: System.out.println("mossa non valida, Immetti un mossa consentita");
+				
+		}
+	}while(move!= Mossa.SASSO || move!= Mossa.CARTA || move!= Mossa.FORBICI);		
+		
+		return move;
+		
+	}
+
+}
